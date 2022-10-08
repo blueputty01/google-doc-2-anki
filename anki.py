2533b3aa21a22f33e9839bc8d113826418bf455d
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-10-08 15:25:13 -0400

    Initial commit

---
+++ anki.py
@@ -0,0 +1,52 @@
+import json
+import socket
+import urllib.request
+
+
+def request(action, **params):
+    return {'action': action, 'params': params, 'version': 6}
+
+
+def invoke(action, **params):
+    request_json = json.dumps(request(action, **params)).encode('utf-8')
+    # print(request_json)
+
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    location = ("localhost", 8765)
+    result_of_check = a_socket.connect_ex(location)
+
+    if result_of_check == 0:
+        print("Port is open")
+        response = json.load(urllib.request.urlopen(
+            urllib.request.Request('http://localhost:8765', request_json)))
+
+        if len(response) != 2:
+            raise Exception('response has an unexpected number of fields')
+        if 'error' not in response:
+            raise Exception('response is missing required error field')
+        if 'result' not in response:
+            raise Exception('response is missing required result field')
+        if response['error'] is not None:
+            raise Exception(response['error'])
+        return response['result']
+    else:
+        print("Port is not open")
+        return
+
+
+def auto_send(notes):
+    # actions = [request('addNotes', notes=notes)]
+    result = invoke('addNotes', notes=notes)
+    rejected = []
+
+    total = len(notes)
+    for j, note in enumerate(notes):
+        if result is None or result[j] is None:
+            rejected.append(f'{note["fields"]} under {note["deckName"]}')
+
+    rej_count = len(rejected)
+    print(f'{total - rej_count} / {total} notes were successfully added to Anki.')
+    if rej_count > 0:
+        print("The following notes were rejected by Anki:")
+        print(*rejected, sep='\n')
---
+++ parser.py
@@ -0,0 +1,344 @@
+import copy
+import hashlib
+import logging
+import os.path
+import platform
+import re
+import shutil
+import zipfile
+from pathlib import Path
+
+import cssutils
+import unicodedata
+from bs4 import BeautifulSoup, NavigableString
+
+import anki
+
+INPUT_FILE = None
+
+SYSTEM = platform.system()
+if SYSTEM == "Linux":
+    INPUT_FILE = Path("/home/alex/desktop/Anki Cards.zip")
+else:
+    INPUT_FILE = Path("D:/Desktop/Anki Cards.zip")
+
+DECK_NAME_DICT = {
+    'APUSH': 'AP US History',
+    'Stat': 'AP Stat',
+    'Calc': 'Math',
+    'Micro': 'AP Micro'
+}
+
+CLASS_DICT = {
+    'UNDERLINE': 'NOT FOUND',
+    'BOLD': 'NOT FOUND',
+    'ITALIC': 'NOT FOUND'
+}
+
+cssutils.log.setLevel(logging.CRITICAL)
+
+cloze_idx = 1
+indents = []
+extracted_loc = Path()
+
+
+def hash_file(path):
+    BUFF_SIZE = 65536  # read in 64kb chunks
+
+    sha1 = hashlib.sha1()
+
+    with open(path, 'rb') as f:
+        while True:
+            data = f.read(BUFF_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+
+    return sha1.hexdigest()
+
+
+# returns a list of indent utility classes in increasing indent size
+def parse_styles(page):
+    global indents
+
+    for styles in page.find_all('style'):
+        css = cssutils.parseString(styles.encode_contents())
+        for rule in css:
+            if rule.type == rule.STYLE_RULE:
+                style_name = rule.selectorText[1:]  # remove . class selector
+
+                if style_name.startswith('c'):
+                    if rule.style.textDecoration == 'underline':
+                        CLASS_DICT['UNDERLINE'] = style_name
+                    elif rule.style.fontWeight == '700':
+                        CLASS_DICT['BOLD'] = style_name
+                    elif rule.style.fontStyle == 'italic':
+                        CLASS_DICT['ITALIC'] = style_name
+
+                    # look for indentation
+                    indent = rule.style.marginLeft
+                    if indent == '':
+                        # this is not an indent style
+                        continue
+                    indent = int(re.sub('\\D', '', indent))
+                    indents.append({'name': style_name, 'indent': indent})
+    indents = sorted(indents, key=lambda x: x['indent'])
+    indents = [indent['name'] for indent in indents]
+
+
+def get_deck_name(tag):
+    global DECK_NAME_DICT
+
+    try:
+        tag_root = tag.split('::')[0].replace('#', '')
+        deck_name = DECK_NAME_DICT[tag_root]
+    except AttributeError:
+        deck_name = 'Default'
+        print('A card was discovered without a deck name. It will be added to the deck called Default')
+
+    return deck_name
+
+
+def check_extra(ele):
+    return hasattr(ele, 'attrs') and 'class' in ele.attrs and not set(ele['class']).isdisjoint(indents)
+
+
+def parse_element(ele):
+    global cloze_idx
+
+    if isinstance(ele, NavigableString):
+        return ele, False, None
+
+    extra = check_extra(ele)
+
+    if ele.name in ['span', 'p']:
+        text, e, media = parse_list(ele)
+        if e:
+            extra = True
+        if 'class' in ele.attrs:
+            if CLASS_DICT['UNDERLINE'] in ele['class']:
+                if re.match('^\\d+::', text):
+                    local_cloze_idx = int(text.split('::')[0])
+                    cloze_idx = local_cloze_idx
+                else:
+                    text = f'{cloze_idx}::{text}'
+                cloze_idx += 1
+                text = f'{{{{c{text}}}}}'
+            if CLASS_DICT['BOLD'] in ele['class']:
+                text = f'<b>{text}</b>'
+            if CLASS_DICT['ITALIC'] in ele['class']:
+                text = f'<i>{text}</i>'
+        if ele.name == 'p':
+            text += '\n'
+        return text, extra, media
+
+    soup = BeautifulSoup()
+    new_tag = soup.new_tag(ele.name)
+
+    if ele.name == "img":
+        global extracted_loc
+
+        src_string = ele['src']
+
+        img_path = str(extracted_loc / src_string)
+        _, extension = os.path.splitext(src_string)
+        image_id = hash_file(img_path)
+
+        filename = image_id + extension
+
+        this_data = {'path': img_path, 'filename': filename}
+        this_media = {'type': 'picture', 'data': this_data}
+        media = [this_media]
+        new_tag['src'] = f"{filename}"
+    else:
+        text, e, media = parse_list(ele.children)
+        if e:
+            extra = True
+        new_tag.append(BeautifulSoup(text, features="html.parser"))
+
+    return str(new_tag), extra, media
+
+
+def parse_list(soup_ele):
+    part_list = list(soup_ele)
+    if len(part_list) == 0:
+        return '', None
+
+    is_single = hasattr(soup_ele, 'name')
+
+    media = []
+    parts = []
+    extra = False
+
+    for ele in part_list:
+        p, e, m = parse_element(ele)
+        if e:
+            extra = True
+        parts.append(p)
+        if m is not None and len(m) > 0:
+            media.extend(m)
+    text = ''.join(parts)
+    text = unicodedata.normalize("NFKD", text)
+    if is_single and soup_ele.name == 'p':
+        text += '\n'
+    if check_extra(soup_ele):
+        extra = True
+    if is_single and soup_ele.name in ['ul', 'ol']:
+        text = f'<{soup_ele.name}>{text}</{soup_ele.name}>'
+    return text, extra, media
+
+
+def clean_lists(body):
+    global indents
+
+    new_file = BeautifulSoup()
+    html_list = None
+    level_offset = 0
+
+    def add_html_list():
+        nonlocal html_list
+        new_file.append(html_list)
+        html_list = None
+        pass
+
+    for b in body:
+        if b.name not in ['ol', 'ul']:
+            if html_list is not None:
+                add_html_list()
+            new_file.append(copy.copy(b))
+            continue
+
+        # get level
+        first_child = list(b.children)[0]
+        current_level = -1
+        for c in first_child['class']:
+            try:
+                local_idx = indents.index(c)
+                current_level = local_idx
+            except ValueError:
+                pass
+
+        if html_list is None:
+            level_offset = current_level
+            html_list = copy.copy(b)
+            continue
+
+        # https://peps.python.org/pep-0448/
+        parent = html_list
+        nesting = current_level - level_offset
+        for idx in range(nesting):
+            parent = parent.contents[-1]
+
+        if nesting == 0:
+            parent.extend(copy.copy(b))
+        else:
+            parent.append(copy.copy(b))
+
+    if html_list is not None:
+        add_html_list()
+
+    return new_file
+
+
+def parse_file(soup):
+    notes = []
+
+    tag = 'no-tag'
+
+    def new_note():
+        global cloze_idx
+        notes.append({'text': '', 'extra': '', 'tags': [tag], 'media': [], 'deck': get_deck_name(tag)})
+        cloze_idx = 1
+
+    body = soup.find("body").children
+
+    body = clean_lists(body)
+
+    for b in body:
+        if b.text.strip() == '':
+            if b.find('img') is None:
+                # line break
+                new_note()
+                continue
+        if 'title' in b['class']:
+            tag = b.text
+            new_note()
+        else:
+            if len(notes) == 0:
+                new_note()
+            text, extra, m = parse_list(b)
+            notes[-1]['extra' if extra else 'text'] += text
+            if m is not None and len(m) > 0:
+                notes[-1]['media'].extend(m)
+
+    to_return = []
+    for note in notes:
+        if note['text'] == '':
+            continue
+
+        for field in ['text', 'extra']:
+            note[field] = note[field].strip().replace('\n', '<br>')
+
+        if '{{c' not in note['text']:
+            print('A card was discovered without a cloze deletion.')
+            print(note)
+            note['text'] += '{{c1::}}'
+
+        media_to_send = {}
+        for m in note['media']:
+            media_to_send[m['type']] = m['data']
+
+        rearranged = {'deckName': note['deck'],
+                      'modelName': "cloze",
+                      'fields': {'Text': note['text'], 'Extra': note['extra']},
+                      'tags': note['tags'],
+                      "options": {
+                          "allowDuplicate": False,
+                          "duplicateScope": note['deck'],
+                          "duplicateScopeOptions": {
+                              "deckName": note['deck'],
+                              "checkChildren": False,
+                              "checkAllModels": False
+                          }
+                      },
+                      **media_to_send
+                      }
+        to_return.append(rearranged)
+    return to_return
+
+
+def parse():
+    global INPUT_FILE, extracted_loc
+
+    extracted_locs = []
+
+    try:
+        archive = zipfile.ZipFile(INPUT_FILE, "r")
+    except FileNotFoundError:
+        print("File not found")
+        return
+    file_name = archive.namelist()[0]
+    extracted_loc = INPUT_FILE.parents[0] / INPUT_FILE.stem
+    archive.extractall(extracted_loc)
+
+    extracted_locs.append(extracted_loc)
+
+    with archive.open(file_name, mode="r") as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+        parse_styles(soup)
+        notes = parse_file(soup)
+
+    return notes, extracted_locs
+
+
+if __name__ == "__main__":
+    to_add, to_remove = parse()
+    print()
+    anki.auto_send(to_add)
+
+    for loc in to_remove:
+        shutil.rmtree(loc)
+
+    i = input("Delete? (Y/n) ")
+    if i == "Y":
+        os.remove(INPUT_FILE)
